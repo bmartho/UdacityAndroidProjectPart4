95778b0fe5c70a331ecdf54002158a5ed8d8f933
Bruno Martho <bjmartho@msn.com>
2022-08-01 14:13:29 -0300

    Implementing tests for  ReminderListFragment

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -2,18 +2,27 @@ package com.udacity.project4.locationreminders.reminderslist
 
 import android.content.Context
 import android.os.Bundle
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.util.BaseTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -21,11 +30,98 @@ import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
-//UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest : BaseTest() {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+    @Test
+    fun clickTask_navigateToSaveReminderFragment() = runBlockingTest {
+        // GIVEN
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // WHEN
+        onView(withId(R.id.addReminderFAB))
+            .perform(click())
+
+        // THEN
+        verify(navController).navigate(
+            ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
+    @Test
+    fun clickTask_navigateToLogout() = runBlockingTest {
+        // GIVEN
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        // WHEN
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext);
+        onView(
+            withText(
+                getApplicationContext<Context>().getString(
+                    R.string.logout
+                )
+            )
+        ).perform(click())
+
+        // THEN
+        onView(withId(R.id.loginButton)).check(matches(isDisplayed()))
+        onView(
+            withText(
+                getApplicationContext<Context>().getString(
+                    R.string.welcome_to_the_location_reminder_app
+                )
+            )
+        ).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun reminderList_DisplayedInUi() {
+        //GIVEN
+        val title = "title"
+        val description = "description"
+        val location = "location"
+        val latitude = 1.0
+        val longitude = 2.0
+
+        val reminderDTO = ReminderDTO(
+            title = title,
+            description = description,
+            location = location,
+            latitude = latitude,
+            longitude = longitude
+        )
+        runBlocking {
+            repository.saveReminder(reminderDTO)
+        }
+
+        // WHEN
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        // THEN
+        onView(withId(R.id.noDataTextView)).check(matches(not(isDisplayed())))
+        onView(withText(reminderDTO.title)).check(matches(isDisplayed()))
+        onView(withText(reminderDTO.description)).check(matches(isDisplayed()))
+        onView(withText(reminderDTO.location)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun reminderList_noData_DisplayedInUi() {
+        //GIVEN
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+
+        // WHEN
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        // THEN
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+    }
 }
---
+++ app/src/androidTest/java/com/udacity/project4/util/BaseTest.kt
@@ -0,0 +1,61 @@
+package com.udacity.project4.util
+
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+
+open class BaseTest :
+    AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
+
+    lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
+
+    /**
+     * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
+     * at this step we will initialize Koin related code to be able to use it in out testing.
+     */
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
+
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
+}
