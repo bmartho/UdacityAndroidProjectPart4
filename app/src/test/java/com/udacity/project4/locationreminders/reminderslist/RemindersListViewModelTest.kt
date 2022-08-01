ac4deff4eefbc3aecbdcf85b384b181b19a2b32e
Bruno Martho <bjmartho@msn.com>
2022-07-31 22:49:39 -0300

    Implementing tests for RemindersListViewModel

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,127 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.utils.MainCoroutineRule
+import com.udacity.project4.locationreminders.utils.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    lateinit var remindersListViewModel: RemindersListViewModel
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private val fakeDataSource = FakeDataSource()
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setUp() {
+        remindersListViewModel = RemindersListViewModel(getApplicationContext(), fakeDataSource)
+    }
+
+    @Test
+    fun getErrorResultFromLoadReminders() = runBlockingTest {
+        // GIVEN
+        fakeDataSource.deleteAllReminders()
+
+        // WHEN
+        remindersListViewModel.loadReminders()
+
+        // THEN
+        assertThat(
+            remindersListViewModel.showSnackBar.getOrAwaitValue(),
+            Matchers.`is`("error")
+        )
+
+        assertThat(
+            remindersListViewModel.showNoData.getOrAwaitValue(),
+            Matchers.`is`(true)
+        )
+    }
+
+    @Test
+    fun getSuccessResultFromLoadReminders() = runBlockingTest {
+        // GIVEN
+        val title = "title"
+        val description = "description"
+        val location = "location"
+        val latitude = 1.0
+        val longitude = 2.0
+
+        val reminderDTO = ReminderDTO(
+            title = title,
+            description = description,
+            location = location,
+            latitude = latitude,
+            longitude = longitude
+        )
+
+        fakeDataSource.saveReminder(reminderDTO)
+
+        // WHEN
+        remindersListViewModel.loadReminders()
+
+        // THEN
+        assertThat(
+            remindersListViewModel.remindersList.getOrAwaitValue()[0].title,
+            Matchers.`is`(title)
+        )
+        assertThat(
+            remindersListViewModel.remindersList.getOrAwaitValue()[0].description,
+            Matchers.`is`(description)
+        )
+        assertThat(
+            remindersListViewModel.remindersList.getOrAwaitValue()[0].location,
+            Matchers.`is`(location)
+        )
+        assertThat(
+            remindersListViewModel.showNoData.getOrAwaitValue(),
+            Matchers.`is`(false)
+        )
+    }
+
+    @Test
+    fun loadingTest() {
+        // GIVEN
+        mainCoroutineRule.pauseDispatcher()
+
+        // WHEN
+        remindersListViewModel.loadReminders()
+
+        // THEN
+        assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            Matchers.`is`(true)
+        )
+
+        mainCoroutineRule.resumeDispatcher()
+
+        assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            Matchers.`is`(false)
+        )
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
 }
